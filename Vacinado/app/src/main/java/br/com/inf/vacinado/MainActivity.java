7972fc5ba84c3f40eef4db809968f4e1f51eb80a
koziel101 <hiagokoziel100@gmail.com>
2017-05-14 23:10:36 -0300

    Incluindo persistencia do login de modo offline

--- Vacinado/app/src/main/java/br/com/inf/vacinado/MainActivity.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/MainActivity.java
@@ -1,14 +1,15 @@
 package br.com.inf.vacinado;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected EditText passwordEditText;
     private FirebaseAuth mFirebaseAuth;
     MaterialDialog dialog;
+    CompoundButton checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +46,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         emailEditText = (EditText) findViewById(R.id.edtEmail);
         passwordEditText = (EditText) findViewById(R.id.edtSenha);
+
+        checkBox = (CompoundButton) findViewById(R.id.checkBoxLembrar);
+
+        SharedPreferences prefs = getSharedPreferences("login", MODE_PRIVATE);
+        if (prefs != null) {
+
+            if (prefs.getBoolean("lembrar", false) == true) {
+                emailEditText.setText(prefs.getString("email", ""));
+                passwordEditText.setText(prefs.getString("senha", ""));
+                checkBox.setChecked(true);
+            }
+        }
     }
 
     @Override
@@ -71,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
+                                        persistirLogin();
                                         dialog.dismiss();
                                         Intent intent = new Intent(MainActivity.this, Carteira.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -86,5 +101,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+    }
+
+    private void persistirLogin() {
+
+        SharedPreferences prefs = getSharedPreferences("login", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        if (checkBox.isChecked()) {
+            editor.putString("email", emailEditText.getText().toString());
+            editor.putString("senha", passwordEditText.getText().toString());
+            editor.putBoolean("lembrar", true);
+        } else {
+            editor.putBoolean("lembrar", false);
+        }
+        editor.apply();
     }
 }
