8f80348eb0928f4c98741a7211e30171a1bd7aff
Matheus Cardoso Duarte Santos <mtesedd@hotmail.com>
2017-05-15 03:03:46 -0300

    Adiciona View de informações da vacina
    Adiciona View de informações da vacina já tomada

--- Vacinado/app/src/main/java/br/com/inf/vacinado/Carteira.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/Carteira.java
@@ -1,7 +1,11 @@
 package br.com.inf.vacinado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 
 public class Carteira extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Carteira extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carteira);
+
+        final CardView button = (CardView) findViewById(R.id.card_view1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                vacinaInfo();
+            }
+        });
+    }
+
+    private void vacinaInfo(){
+        Intent intent = new Intent(Carteira.this, VacinaInfo.class);
+        startActivity(intent);
     }
 }
---
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/VacinaInfo.java
@@ -0,0 +1,33 @@
+package br.com.inf.vacinado;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class VacinaInfo extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacina_tomada_info);
+
+        final Button button = (Button) findViewById(R.id.vacinaVoltar);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                carteira();
+            }
+        });
+
+        ListView listaDeCursos = (ListView) findViewById(R.id.listaDoses);
+    }
+
+    private void carteira(){
+        Intent intent = new Intent(VacinaInfo.this, Carteira.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+}
