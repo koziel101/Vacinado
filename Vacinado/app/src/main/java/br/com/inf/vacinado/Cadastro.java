f9b1463f26316d98132e47110014d4dd32a2306d
koziel101 <hiagokoziel100@gmail.com>
2017-05-15 20:13:39 -0300

    Desacoplando classe "Cadastro": Validação dos dados cadastrais movido para outra classe

--- Vacinado/app/src/main/java/br/com/inf/vacinado/Cadastro.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/Cadastro.java
@@ -5,12 +5,10 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringDef;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -28,6 +26,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.util.Calendar;
 
 import br.com.inf.vacinado.Controller.MascaraCpf;
+import br.com.inf.vacinado.Controller.Validador;
 import br.com.inf.vacinado.DAO.UsuarioDAO;
 import br.com.inf.vacinado.Model.UsuarioInfo;
 
@@ -44,7 +43,7 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
     protected EditText passwordEditText;
     protected EditText emailEditText;
     protected static EditText edit_cpf;
-    protected Boolean checkNascimento = false;
+    protected static Boolean checkNascimento = false;
     protected Spinner spinner;
 
     private FirebaseAuth mFirebaseAuth;
@@ -52,11 +51,6 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
 
     private MaterialDialog dialog;
 
-
-
-    public static void setIsUpdating(boolean isUpdating) {
-        Cadastro.isUpdating = isUpdating;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +61,8 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
         configuraSpinner(spinner);
 
         showDialog();
+
+        nomeEditText = (EditText) findViewById(R.id.edit_nome_cadastro);
 
         edit_cpf = (EditText) findViewById(R.id.edt_cpf_cadastro);
 
@@ -107,6 +103,13 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
 
     }
 
+    public static void setIsUpdating(boolean isUpdating) {
+        Cadastro.isUpdating = isUpdating;
+    }
+
+    public static Boolean getCheckNascimento() {
+        return checkNascimento;
+    }
 
     private DatePickerDialog.OnDateSetListener dPickerListener = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -169,10 +172,7 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
 
             case R.id.bttnFinalizar:
 
-                int validador = validaDados();
-
-                Log.e("CPF: ", edit_cpf.getText().toString());
-                Log.e("Validador: ", String.valueOf(validador));
+                int validador = Validador.validaDados(nomeEditText, emailEditText, passwordEditText, edit_cpf);
 
                 if (validador == 0) {
 
@@ -227,48 +227,4 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
                 }
         }
     }
-
-    private int validaDados() {
-
-        nomeEditText = (EditText) findViewById(R.id.edit_nome_cadastro);
-        String nome = nomeEditText.getText().toString();
-
-        if (nome.isEmpty()) {
-            return 1;
-        }
-
-        String email = emailEditText.getText().toString().trim();
-
-        if (email.isEmpty()) {
-            return 2;
-        }
-
-        String password = passwordEditText.getText().toString().trim();
-
-        if (password.length() < 6) {
-            return 3;
-        }
-
-        if (verificaCpf() == false) {
-            return 4;
-        }
-        if (checkNascimento == false) {
-            return 5;
-        }
-
-        return 0;
-
-    }
-
-    private boolean verificaCpf() {
-
-        String cpf = edit_cpf.getText().toString();
-
-        if (cpf.isEmpty() || cpf.length() < 14) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
 }
--- Vacinado/app/src/main/java/br/com/inf/vacinado/Controller/Validador.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/Controller/Validador.java
@@ -1,4 +1,55 @@
 package br.com.inf.vacinado.Controller;
 
+import android.widget.EditText;
+
+import br.com.inf.vacinado.Cadastro;
+import br.com.inf.vacinado.R;
+
 public class Validador {
+
+
+    public static int validaDados(EditText nomeEditText, EditText emailEditText, EditText passwordEditText, EditText edit_cpf) {
+
+
+        String nome = nomeEditText.getText().toString();
+
+        if (nome.isEmpty()) {
+            return 1;
+        }
+
+        String email = emailEditText.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            return 2;
+        }
+
+        String password = passwordEditText.getText().toString().trim();
+
+        if (password.length() < 6) {
+            return 3;
+        }
+
+        if (verificaCpf(edit_cpf) == false) {
+            return 4;
+        }
+        if (Cadastro.getCheckNascimento() == false) {
+            return 5;
+        }
+
+        return 0;
+
+    }
+
+    private static boolean verificaCpf(EditText edit_cpf) {
+
+        String cpf = edit_cpf.getText().toString();
+
+        if (cpf.isEmpty() || cpf.length() < 14) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
 }
