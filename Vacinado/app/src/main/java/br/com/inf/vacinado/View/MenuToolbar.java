00a5c594965c668af3cb35e588f0a06a4005a940
Marjorie Celestino <marjorie_celestino@hotmail.com>
2017-05-27 16:22:50 -0300

    Adicionado código inicial da toolbar
    Adicionado código inicial para adição da toolbar nos layouts.

---
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/View/MenuToolbar.java
@@ -0,0 +1,51 @@
+package br.com.inf.vacinado.View;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.inf.vacinado.R;
+
+
+/**
+ * Created by marge on 27/05/2017.
+ */
+
+public class MenuToolbar extends ActionBarActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Une o layout ao toolbar
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //no inspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
