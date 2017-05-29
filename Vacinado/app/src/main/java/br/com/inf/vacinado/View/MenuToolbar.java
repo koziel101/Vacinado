fd7a51f8d4e24a378c2cabf912ee964a924da237
Marjorie Celestino <marjorie_celestino@hotmail.com>
2017-05-29 17:00:52 -0300

    Classe referente ao menu

--- Vacinado/app/src/main/java/br/com/inf/vacinado/View/MenuToolbar.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/View/MenuToolbar.java
@@ -1,6 +1,6 @@
 package br.com.inf.vacinado.View;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,39 +13,36 @@ import br.com.inf.vacinado.R;
  * Created by marge on 27/05/2017.
  */
 
-public class MenuToolbar extends ActionBarActivity {
+public class MenuToolbar extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.tool_bar);
 
         //Une o layout ao toolbar
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
     }
 
-   /* @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // adiciona itens a actionbar
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        //seleção de item na actionbar
         int id = item.getItemId();
 
-        //no inspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
