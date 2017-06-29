b9f59c3da16992a5e97766c947ee4f74c702ab26
Marjorie Celestino <marjorie_celestino@hotmail.com>
2017-06-28 22:21:19 -0300

    Criação dinâmica de cardview
    Iniciado código para adição dinâmica de cardview ao adicionar uma nova
    vacina.

--- Vacinado/app/src/main/java/br/com/inf/vacinado/View/AdicionarVacina.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/View/AdicionarVacina.java
@@ -1,12 +1,17 @@
 package br.com.inf.vacinado.View;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import br.com.inf.vacinado.DAO.VacinaDAO;
 import br.com.inf.vacinado.Model.Vacina;
@@ -17,19 +22,57 @@ public class AdicionarVacina extends AppCompatActivity {
     protected EditText nomeVacinaEdt;
     protected EditText vacinaQntdEdt;
     protected EditText informacoesVacinaEdt;
+    LinearLayout listaVacinas;
+    private Context contextoCarteira;
+    Carteira carteira;
+    LinearLayout listaCard;
+    protected int WIDTH_CARD = 50;
+    Vacina vacina;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vacina);
 
+        contextoCarteira = carteira.contextoAplicacao();
+
+        //referências layout xml para lista de cards de vacina
+        listaCard = (LinearLayout) findViewById(R.id.lista_vacinas);
+
         final Button concluir = (Button) findViewById(R.id.bttConcluir);
         concluir.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 concluir();
+
+                CardView card = new CardView(contextoCarteira);
+
+                //Parâmetros do CardView
+                CardView.LayoutParams params = new CardView.LayoutParams(
+                        CardView.LayoutParams.WRAP_CONTENT,
+                        CardView.LayoutParams.MATCH_PARENT
+                );
+                params.setMargins(10,10,10,10);
+                card.setLayoutParams(params);
+
+                card.setRadius(2);
+                card.setMaxCardElevation(2);
+                card.setMinimumHeight(50);
+
+                //Cria TextView e adiciona ao Card
+                TextView nomeVacina = new TextView(contextoCarteira);
+                params.setMargins(20,0,0,0);
+                nomeVacina.setLayoutParams(params);
+                nomeVacina.setText(vacina.getNome());
+                nomeVacina.setGravity(Gravity.CENTER_VERTICAL);
+
+                card.addView(nomeVacina);
+
+                listaCard.addView(card);
+
+                voltarTelaCarteira();
             }
         });
-
     }
 
     private void concluir() {
@@ -45,14 +88,16 @@ public class AdicionarVacina extends AppCompatActivity {
         } else if (informacoesVacinaEdt.getText().toString().trim().isEmpty()) {
             Snackbar.make(findViewById(android.R.id.content), R.string.informacoes_vacina_vazio, Snackbar.LENGTH_LONG).show();
         } else {
-            Vacina vacina = new Vacina(nomeVacinaEdt.getText().toString(),
+            vacina = new Vacina(nomeVacinaEdt.getText().toString(),
                     Integer.parseInt(vacinaQntdEdt.getText().toString()), informacoesVacinaEdt.getText().toString());
             VacinaDAO.persistirVacina(vacina);
-
-            Intent intent = new Intent(AdicionarVacina.this, Carteira.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
         }
+    }
+
+    private void voltarTelaCarteira(){
+        Intent intent = new Intent(AdicionarVacina.this, Carteira.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
