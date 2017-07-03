f8b1fb5a12582451964c11dae08c7e7d4a8535b1
Matheus Cardoso Duarte Santos <mtesedd@hotmail.com>
2017-07-03 20:10:40 -0300

    Adicionando tela de adicionar notificação

--- VacinadoADM/app/src/main/java/com/xpmets/vacinadoadm/Model/Notificacao.java
+++ VacinadoADM/app/src/main/java/com/xpmets/vacinadoadm/Model/Notificacao.java
@@ -1,8 +1,54 @@
 package com.xpmets.vacinadoadm.Model;
 
-/**
- * Created by Matheus on 03/07/2017.
- */
+import java.util.Date;
 
 public class Notificacao {
+
+    private String nome;
+    private String texto;
+    private Date dataInicial;
+    private Date dataFinal;
+
+    public Notificacao() {
+
+    }
+
+    public Notificacao(String nome, String texto, Date dataInicial, Date dataFinal){
+        this.nome = nome;
+        this.texto = texto;
+        this.dataInicial = dataInicial;
+        this.dataFinal = dataFinal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
 }
--- VacinadoADM/app/src/main/java/com/xpmets/vacinadoadm/Model/Vacina.java
+++ VacinadoADM/app/src/main/java/com/xpmets/vacinadoadm/Model/Vacina.java
@@ -1,8 +1,64 @@
 package com.xpmets.vacinadoadm.Model;
 
-/**
- * Created by Matheus on 03/07/2017.
- */
-
 public class Vacina {
+
+    private String id;
+    private String nome;
+    private String informacoes;
+    private int quantidadeDoses;
+    private int dosesTomadas = 0;
+    private String classificacao;
+
+    public Vacina() {
+
+    }
+
+    public Vacina(String nome, int quantidadeDoses, String informacoes, String classificacao) {
+        this.nome = nome;
+        this.quantidadeDoses = quantidadeDoses;
+        this.informacoes = informacoes;
+        this.classificacao = classificacao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getInformacoes() {
+        return informacoes;
+    }
+
+    public void setInformacoes(String informacoes) {
+        this.informacoes = informacoes;
+    }
+
+    public int getQuantidadeDoses() {
+        return quantidadeDoses;
+    }
+
+    public void setQuantidadeDoses(int quantidadeDoses) {
+        this.quantidadeDoses = quantidadeDoses;
+    }
+
+    public int getDosesTomadas() {
+        return dosesTomadas;
+    }
+
+    public void setDosesTomadas(int dosesTomadas) {
+        this.dosesTomadas = dosesTomadas;
+    }
+
 }
+
