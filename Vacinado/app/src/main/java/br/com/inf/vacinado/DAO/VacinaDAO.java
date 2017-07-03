35491057e05385f314675ce9c228102703568897
Matheus Cardoso Duarte Santos <mtesedd@hotmail.com>
2017-07-03 17:20:24 -0300

    Adicionando método de recuperar vacina pelo id

--- Vacinado/app/src/main/java/br/com/inf/vacinado/DAO/VacinaDAO.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/DAO/VacinaDAO.java
@@ -2,33 +2,51 @@ package br.com.inf.vacinado.DAO;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.inf.vacinado.Model.Vacina;
 
 public class VacinaDAO {
 
     //Variaveis para realizar a autenticacao
-    static private FirebaseAuth mFirebaseAuth;
-    static private FirebaseUser mFirebaseUser;
+    static private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+    static private FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
     //Variaveis para persistir dados
-    static private DatabaseReference mDatabase;
-    static private String mUserId;
+    static private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    static private String mUserId = mFirebaseUser.getUid();
+    private DatabaseReference ref = mDatabase.child("users").child(mUserId).child("vacinas");
+    private Vacina vacinaG;
 
     public static void persistirVacina(Vacina vacina) {
-
-        // Initialize Firebase Auth and Database Reference
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        mUserId = mFirebaseUser.getUid();
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
         String id = mDatabase.child("users").child(mUserId).child("vacinas").child("id").push().getKey();
         vacina.setId(id);
         mDatabase.child("users").child(mUserId).child("vacinas").child(id).setValue(vacina);
         mDatabase.keepSynced(true);
+    }
+
+    public Vacina findVacinaById(FirebaseAuth.AuthStateListener mAuthListener, final String id) {
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot vacSnapshot : dataSnapshot.getChildren()) {
+                    Vacina vacina = vacSnapshot.getValue(Vacina.class);
+                    if (vacina.getId() == id){
+                        vacinaG = vacina;
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    return vacinaG;
     }
 }
