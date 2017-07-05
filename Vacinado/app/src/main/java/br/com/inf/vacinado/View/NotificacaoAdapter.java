2a87cd3a358f32b7a220d476f4aa3672fa35c24f
Marjorie Celestino <marjorie_celestino@hotmail.com>
2017-07-05 14:28:51 -0300

    Referênciadas view adicionadas no card

--- Vacinado/app/src/main/java/br/com/inf/vacinado/View/NotificacaoAdapter.java
+++ Vacinado/app/src/main/java/br/com/inf/vacinado/View/NotificacaoAdapter.java
@@ -13,24 +13,28 @@ import java.util.List;
 import br.com.inf.vacinado.Model.Notificacao;
 import br.com.inf.vacinado.R;
 
-public class NotificacaoAdapter extends RecyclerView.Adapter<NotificacaoAdapter.NotificacaoViewHolder>{
+public class NotificacaoAdapter extends RecyclerView.Adapter<NotificacaoAdapter.NotificacaoViewHolder> {
 
     List<Notificacao> notificacoes;
 
-    public NotificacaoAdapter( List<Notificacao> notificacoes){
+    public NotificacaoAdapter(List<Notificacao> notificacoes) {
         this.notificacoes = new ArrayList<>();
         this.notificacoes = notificacoes;
     }
 
-    public static class NotificacaoViewHolder extends RecyclerView.ViewHolder{
+    public static class NotificacaoViewHolder extends RecyclerView.ViewHolder {
 
         CardView cv;
         TextView nome_notificacao;
+        TextView texto_notificacao;
+        TextView data_notificacao;
 
         public NotificacaoViewHolder(View itemView) {
             super(itemView);
             cv = (CardView) itemView.findViewById(R.id.card_view_notification);
             nome_notificacao = (TextView) itemView.findViewById(R.id.notificacaoNome);
+            texto_notificacao = (TextView) itemView.findViewById(R.id.notificacaoTexto);
+            data_notificacao = (TextView) itemView.findViewById(R.id.notificacaoData);
         }
     }
 
@@ -46,6 +50,8 @@ public class NotificacaoAdapter extends RecyclerView.Adapter<NotificacaoAdapter.
     @Override
     public void onBindViewHolder(NotificacaoViewHolder holder, int position) {
         holder.nome_notificacao.setText(notificacoes.get(position).getTitulo());
+        holder.texto_notificacao.setText(notificacoes.get(position).getTexto());
+        holder.data_notificacao.setText(notificacoes.get(position).getDia() + "/" + notificacoes.get(position).getMes() + "/" + notificacoes.get(position).getAno());
     }
 
     @Override
